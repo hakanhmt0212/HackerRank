723db153b7f9aa469e9c7d30421725138d858f85
Ahmet Hakan Demirel <05190000043@ogrenci.ege.edu.tr>
2022-03-19 00:18:40 +0300

    Create Number-Line-Jumps.py

---
+++ Number Line Jumps/Number-Line-Jumps.py
@@ -0,0 +1,24 @@
+def kangaroo(x1, v1, x2, v2):
+    if(v2 > v1):
+        return "NO"
+    else:
+        if (v1 != v2) and (x2 - x1) % (v2 - v1) == 0:
+            return "YES"
+        else:
+            return "NO"
+
+if __name__ == '__main__':
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    print(result)
